159f00184ecfb6cd5ffbce7819efb4f4a365835b
francisca hernandez <francisca.hernandez17@gmail.com>
2022-08-13 13:09:48 -0700

    edited folder

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ function generatePassword() {
   const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '/', '*', '?'];
-  let gatherAnswers= []
+  let gatherAnswers = []
 
 
   //prompt for length
@@ -14,22 +14,22 @@ function generatePassword() {
   length = prompt("How many characters would you like your password to contain?");
   if (length < 8 || length > 128) {
     return "Password does not meet minimum length criteria";
-  } 
+  }
   else if (isNaN(length)) {
     length = prompt("please enter a valid number.")
     generatePassword();
-   }
+  }
   else {
     alert(" Your password will be " + length + " characters long.");
   }
 
 
-    // prompt questions 
-    let hasNumbers = confirm('Do you want numbers?');
-    let hasUpperCase = confirm('Do you want uppercase letters?')
-    let hasLowerCase = confirm('Do you want lower case letters?');
-    let hasSpecialCharacters = confirm('Do you want special characters?');
-  
+  // prompt questions 
+  let hasNumbers = confirm('Do you want numbers?');
+  let hasUpperCase = confirm('Do you want uppercase letters?')
+  let hasLowerCase = confirm('Do you want lower case letters?');
+  let hasSpecialCharacters = confirm('Do you want special characters?');
+
 
   //Selected Characters
 
@@ -50,20 +50,20 @@ function generatePassword() {
       alert('Password does not meet minimum requirements')
   }
 
- 
+
   let results = []
-    for(let i=0; i<length; i++) {
-      let randomIndex = Math.floor(Math.random() * gatherAnswers.length)
-      let randomContent = gatherAnswers[randomIndex];
-      results.push(randomContent);
-    
-    }
+  for (let i = 0; i < length; i++) {
+    let randomIndex = Math.floor(Math.random() * gatherAnswers.length)
+    let randomContent = gatherAnswers[randomIndex];
+    results.push(randomContent);
+
+  }
   return results.join("");
-  
+
   //randomContent= gatherAnswers[length];
 
 
-  }
+}
 
 
 //Get References to the #gerate element
